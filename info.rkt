b5b52912e31d5dc768eb692c48fd558cefdf472e
tahoupt <tom@houptlab.org>
2023-12-26 22:29:22 -0500

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "circular-layout")
 (define version "1.0")
-(define scribblings '(("scribblings/circular-layout.scrbl")))
+(define scribblings '(("scribblings/circular-layout.scrbl" ())))
 (define pkg-desc "A lightweight module for drawing nodes and edges in a circular layout.")
 (define license 'MIT)
 (define pkg-authors '(houpt@bio.fsu.edu))
